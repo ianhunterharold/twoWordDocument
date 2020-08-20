e69b2d4d4218bdf5289765fd9cfb3f69d010f4ff
Ian Harold <ianhunterharold@gmail.com>
2020-08-20 12:09:03 -0600

    wip, working on multi-line input

---
+++ twoWordsDoc.js
@@ -0,0 +1,35 @@
+const twoWords = (wordData) => {
+
+    const longest = wordData.split('\n')[0].toLowerCase();
+    const first = wordData.split('\n')[1].toLowerCase();
+    const last = wordData.split('\n')[2].toLowerCase();
+  
+    const strToArr = longest.split(' '); 
+  
+    const distance = 0;
+  
+    
+    for (let i=0; i <strToArr.length; i++){
+      
+      const indexOfFirst = 0;
+      
+      if (strToArr[i] === first){
+        
+        indexOfFirst = i;
+  
+        for (let j =0; j <strToArr.length; j++){
+          if (strToArr[j] === last){
+            distance = j - indexOfFirst;
+            
+            return distance;
+          }
+        }
+      }
+    }
+  
+  }
+  
+  
+  const input = "Peter piper picked a patch of pickeled peppers and sally sold seashells by the seashore \n peter \n `sally"; 
+  
+  twoWords(input);
